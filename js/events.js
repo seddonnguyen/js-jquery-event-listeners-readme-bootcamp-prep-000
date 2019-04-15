dbd436760f4c3859d7e1a87a4b21cfea1a396a2e
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-15 22:50:27 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,27 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  })
+}
 
+function frameIt() {
+  $('img').addClass("tasty");
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(key) {
+    if(key.which === 71) {
+      alert("Hello");
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
 $(document).ready(function(){
 
 // call functions here
